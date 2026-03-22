898b56c4d97ae77d92dcb125cfb43d89ad3340ea
MasiaSaig <mullermaciej12@gmail.com>
2026-03-22 20:51:29 +0100

    Set compose key

--- home/hyprland/input.nix
+++ home/hyprland/input.nix
@@ -6,7 +6,10 @@
     {
       kb_layout = "us,pl";
       kb_variant = "";
-      kb_options = [ "grp:alt_shift_toggle" ];
+      kb_options = [
+        "grp:alt_shift_toggle"
+	"compose:ralt"
+      ];
       repeat_delay = 300;
       follow_mouse = 1;
       sensitivity = 0;
