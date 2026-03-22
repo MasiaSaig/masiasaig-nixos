9b18e470cd6beba7df105741708634d9ed5c96ac
MasiaSaig <mullermaciej12@gmail.com>
2026-03-22 16:05:01 +0100

    Fix waybar fonts

--- hosts/laptop-dragon/fonts.nix
+++ hosts/laptop-dragon/fonts.nix
@@ -1,7 +1,12 @@
 { config, pkgs, ... }:
 
 {
+  fonts.fontconfig.enable = true;
+
   fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-color-emoji
+    #nerdfonts
     font-awesome
   ];
 }
