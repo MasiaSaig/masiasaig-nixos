2d9f2cd0d5948aa1027d8cf02870abc2f84ff2ce
MasiaSaig <mullermaciej12@gmail.com>
2026-04-17 09:31:15 +0200

    Remove slash from bash PS1

--- home/programs/bash.nix
+++ home/programs/bash.nix
@@ -8,7 +8,7 @@
       parse_git_branch() {
         git branch 2>/dev/null | grep '*' | sed 's/* //'
       }
-      export PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\[\033[01;33m\]\ $(parse_git_branch)\[\033[00m\]\$ "
+      export PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\[\033[01;33m\] \$(parse_git_branch)\[\033[00m\]\$ "
     '';
   };
 }
